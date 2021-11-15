fd703fc48f41b87c5206464f88d4e797203afa4f
shAtud <sidalihouda.computerscience@gmail.com>
2021-11-15 20:54:44 +0100

    aligned

--- toul.js
+++ toul.js
@@ -11,20 +11,34 @@ class Charactere{
         this.r = 0.5*this.b_delta_y;
         this.theta_droite_afous = -45
         this.theta_gauche_afous = 45
-        this.theta_droite_rejlin = -45
-        this.theta_gauche_rejlin = 45
+        this.theta_droite_rejlin = -20
+        this.theta_gauche_rejlin = 20
         this.toulAfous = this.b_delta_y
         this.toulRejlin = 1.5*this.b_delta_y
+
+         // (x,(y+b_delta_y) - toulRejlin*Math.sin(-theta_gauche_rejlin))
+         const last_part = Math.sqrt(
+                Math.pow(this.x-this.x,2)
+                +Math.pow( (this.y+this.b_delta_y)-this.toulRejlin*Math.sin(-this.theta_gauche_rejlin)-(this.y +this.b_delta_y) ,2)
+                );
+        this.khchiba_height = 2*this.r + this.b_delta_y*2 +last_part;
     }
     set_b_delta_y(b_delta_y){
         this.b_delta_y = b_delta_y
         this.r = 0.5*this.b_delta_y;
         this.toulAfous = this.b_delta_y
         this.toulRejlin = 1.5*this.b_delta_y
+
+         // (x,(y+b_delta_y) - toulRejlin*Math.sin(-theta_gauche_rejlin))
+         const last_part = Math.sqrt(
+            Math.pow(this.x-this.x,2)
+            +Math.pow( (this.y+this.b_delta_y)-this.toulRejlin*Math.sin(-this.theta_gauche_rejlin)-(this.y +this.b_delta_y) ,2)
+            );
+    this.khchiba_height = 2*this.r + this.b_delta_y*2 +last_part;
     }
 
     draw(){
-        this.x+=1;
+      
         const {x,y,b_delta_y,r,theta_droite_afous , theta_gauche_afous,theta_droite_rejlin,theta_gauche_rejlin,toulRejlin,toulAfous} = this;
         //5chiba corp
         ctx.beginPath()
@@ -60,22 +74,37 @@ class Charactere{
             ctx.strokeStyle = 'black'
             ctx.stroke()
         ctx.closePath()
-        //khchiba main gauche
+        //khchiba pied gauche
         ctx.beginPath()
             ctx.moveTo(x,(y+b_delta_y))
             ctx.lineTo(x - toulRejlin*Math.cos(theta_gauche_rejlin),(y+b_delta_y) - toulRejlin*Math.sin(-theta_gauche_rejlin))
             ctx.strokeStyle = 'black'
             ctx.stroke()
         ctx.closePath()
+       
         
     }
 }
 
 const khchiba = new Charactere(width/2,height/2,20);
+
+let characters = [];
+const N = 30;
+for(let i = 0 ;i<N ;i++){
+    characters.push(new Charactere((320+width*(i))/N,height-75,Math.random()*15+5))
+}
+characters = characters.sort((a,b)=>{return a.b_delta_y-b.b_delta_y})
+ characters.forEach((el,index)=>{
+    el.x = (320+width*(index))/N;
+    el.y = height - el.khchiba_height/2;
+})
 function animate(){
-    requestAnimationFrame(animate);
+     requestAnimationFrame(animate);
     ctx.clearRect(0,0,width,height);
-    khchiba.draw();
+   
+    for(let i = 0 ;i<N ;i++){
+        characters[i].draw();
+    }
     
 }
 animate();
